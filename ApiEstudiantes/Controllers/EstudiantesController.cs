0faf73b148e4009a6321debfeed318bfa8f7a73c
WillyR777j <72953916+WillyR777j@users.noreply.github.com>
2025-07-19 17:16:27 -0400

    Cambios de prueba

--- ApiEstudiantes/Controllers/EstudiantesController.cs
+++ ApiEstudiantes/Controllers/EstudiantesController.cs
@@ -31,9 +31,9 @@ namespace ApiEstudiantes.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Estudiante>> GetEstudiante(int id)
         {
-            var estudiantes = await _context.Estudiante.FindAsync(id);
+            var estudiante = await _context.Estudiante.FindAsync(id);
 
-            if (estudiantes == null)
+            if (estudiante == null)
                 return NotFound();
 
             return estudiante;
