44f4c1006c848b52e91814b4e96c9b37d3ac99ba
WillyR777j <72953916+WillyR777j@users.noreply.github.com>
2025-07-29 07:39:20 -0400

    Ultimas actualizaciones

--- ApiEstudiantes/Controllers/EstudiantesController.cs
+++ ApiEstudiantes/Controllers/EstudiantesController.cs
@@ -4,6 +4,8 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
 
 namespace ApiEstudiantes.Controllers
 {
@@ -13,30 +15,52 @@ namespace ApiEstudiantes.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly IMapper _mapper;
+        private readonly ILogger<EstudiantesController> _logger;
 
-        public EstudiantesController(ApplicationDbContext context, IMapper mapper)
+        public EstudiantesController(ApplicationDbContext context, IMapper mapper, ILogger<EstudiantesController> logger)
         {
             _context = context;
             _mapper = mapper;
-
+            _logger = logger;
         }
         [HttpGet]
         public async Task<ActionResult<IEnumerable<EstudianteCreacionDTO>>> GetEstudiantes()
         {
-            var estudiantes = await _context.Estudiante.ToListAsync();
-            var estudiantesDto = _mapper.Map<List<EstudianteCreacionDTO>>(estudiantes);
-            return Ok(estudiantesDto);
+            _logger.LogInformation("Obteniendo lista de estudiantes");
+            try
+            {
+                var estudiantes = await _context.Estudiante.ToListAsync();
+                var estudiantesDto = _mapper.Map<List<EstudianteCreacionDTO>>(estudiantes);
+                return Ok(estudiantesDto);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al obtener estudiantes");
+                return StatusCode(500, "Error interno del servidor");
+               
+            }            
         }
        
         [HttpGet("{id}")]
         public async Task<ActionResult<Estudiante>> GetEstudiante(int id)
         {
-            var estudiante = await _context.Estudiante.FindAsync(id);
+            try
+            {
+                var estudiante = await _context.Estudiante.FindAsync(id);
 
-            if (estudiante == null)
-                return NotFound();
-
-            return estudiante;
+                if (estudiante == null)
+                {
+                    _logger.LogInformation("Estudiante no encontrado");
+                }                    
+                    return Ok(estudiante);              
+                
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Estudiante no encontrado");
+                return StatusCode(500, "Error interno del servidor");
+            }
+            
         }
 
         [HttpPost]
--- ApiEstudiantes/obj/Debug/net8.0/ApiEstudiantes.AssemblyInfo.cs
+++ ApiEstudiantes/obj/Debug/net8.0/ApiEstudiantes.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiEstudiantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+041e40ea67341410efdd365fdcced6670f065f1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0faf73b148e4009a6321debfeed318bfa8f7a73c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiEstudiantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiEstudiantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
